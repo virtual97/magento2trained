3d266878a47391ce0dc90f5139a4e4d3601feb93
Sergii Shkiria <s.shkiria@astoundcommerce.com>
2015-10-27 22:03:35 +0200

    2.2.1. Find a place in the code where output is flashed to the browser. Create an extension that captures and logs the file-generated page HTML.

--- app/code/Training/Test/Model/Observer.php
+++ app/code/Training/Test/Model/Observer.php
@@ -1,6 +1,21 @@
 <?php
 namespace Training\Test\Model;
 class Observer {
+    protected $_logger = null;
+
+    public function __construct(\Psr\Log\LoggerInterface $logger)
+    {
+        $this->_logger = $logger;
+    }
+
+    public function logPageOutput(\Magento\Framework\Event\Observer $observer)
+    {
+        return;
+        $response = $observer->getEvent()->getData('response');
+        $body = $response->getBody();
+        $this->_logger->addDebug("--------\n\n\n BODY \n\n\n ". $body);
+    }
+
     public function changeRequestParams(\Magento\Framework\Event\Observer $observer)
     {
         $request = $observer->getEvent()->getData('request');
