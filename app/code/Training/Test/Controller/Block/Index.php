2f7c3dfaef16d6809bd49f3a7d27fc44a24d781b
Sergii Shkiria <s.shkiria@astoundcommerce.com>
2015-11-01 18:33:03 +0200

    3.5.1. Create a block extending AbstractBlock, and implement the _toHtml() method. Render that block in the new controller.

---
+++ app/code/Training/Test/Controller/Block/Index.php
@@ -0,0 +1,11 @@
+<?php
+namespace Training\Test\Controller\Block;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute() {
+        $layout = $this->_view->getLayout();
+        $block = $layout->createBlock('Training\Test\Block\Test');
+        $this->getResponse()->appendBody($block->toHtml());
+    }
+}
