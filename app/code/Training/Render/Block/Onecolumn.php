cd5b5670d41865033255aa44ed14158b01ab7f71
Sergii Shkiria <s.shkiria@astoundcommerce.com>
2015-11-02 20:47:10 +0200

    3.8.7. Using Layout XML, add a new link for the custom page you just created to the set of existing links at the top on every page . -Refactored.

--- app/code/Training/Render/Block/Onecolumn.php
+++ app/code/Training/Render/Block/Onecolumn.php
@@ -5,8 +5,6 @@
  */
 namespace Training\Render\Block;
 
-use Magento\Customer\Model\Context;
-
 /**
  * Customer register link
  *
@@ -15,40 +13,14 @@ use Magento\Customer\Model\Context;
 class Onecolumn extends \Magento\Framework\View\Element\Html\Link
 {
     /**
-     * Customer session
-     *
-     * @var \Magento\Framework\App\Http\Context
-     */
-    protected $httpContext;
-
-//    /**
-//     * @var \Magento\Customer\Model\Registration
-//     */
-//    protected $_registration;
-//
-//    /**
-//     * @var \Magento\Customer\Model\Url
-//     */
-//    protected $_customerUrl;
-
-    /**
      * @param \Magento\Framework\View\Element\Template\Context $context
-     * @param \Magento\Framework\App\Http\Context $httpContext
-     * @param \Magento\Customer\Model\Registration $registration
-     * @param \Magento\Customer\Model\Url $customerUrl
      * @param array $data
      */
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
-        \Magento\Framework\App\Http\Context $httpContext,
-//        \Magento\Customer\Model\Registration $registration,
-//        \Magento\Customer\Model\Url $customerUrl,
         array $data = []
     ) {
         parent::__construct($context, $data);
-        $this->httpContext = $httpContext;
-//        $this->_registration = $registration;
-//        $this->_customerUrl = $customerUrl;
     }
 
     /**
@@ -72,11 +44,6 @@ class Onecolumn extends \Magento\Framework\View\Element\Html\Link
      */
     protected function _toHtml()
     {
-//        if (!$this->_registration->isAllowed()
-//            || $this->httpContext->getValue(Context::CONTEXT_AUTH)
-//        ) {
-//            return '';
-//        }
         return parent::_toHtml();
     }
 }
