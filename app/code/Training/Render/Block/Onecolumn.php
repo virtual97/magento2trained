be9b6b2cf220c99cfd2e3f2ef0c940e8a7c9aff6
Sergii Shkiria <s.shkiria@astoundcommerce.com>
2015-11-02 20:44:48 +0200

    3.8.7. Using Layout XML, add a new link for the custom page you just created to the set of existing links at the top on every page .

---
+++ app/code/Training/Render/Block/Onecolumn.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Training\Render\Block;
+
+use Magento\Customer\Model\Context;
+
+/**
+ * Customer register link
+ *
+ * @SuppressWarnings(PHPMD.DepthOfInheritance)
+ */
+class Onecolumn extends \Magento\Framework\View\Element\Html\Link
+{
+    /**
+     * Customer session
+     *
+     * @var \Magento\Framework\App\Http\Context
+     */
+    protected $httpContext;
+
+//    /**
+//     * @var \Magento\Customer\Model\Registration
+//     */
+//    protected $_registration;
+//
+//    /**
+//     * @var \Magento\Customer\Model\Url
+//     */
+//    protected $_customerUrl;
+
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Framework\App\Http\Context $httpContext
+     * @param \Magento\Customer\Model\Registration $registration
+     * @param \Magento\Customer\Model\Url $customerUrl
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\App\Http\Context $httpContext,
+//        \Magento\Customer\Model\Registration $registration,
+//        \Magento\Customer\Model\Url $customerUrl,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->httpContext = $httpContext;
+//        $this->_registration = $registration;
+//        $this->_customerUrl = $customerUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getHref()
+    {
+        return $this->getUrl('training_render/layout/onecolumn');
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabel()
+    {
+        return __('Training Onecolumn');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function _toHtml()
+    {
+//        if (!$this->_registration->isAllowed()
+//            || $this->httpContext->getValue(Context::CONTEXT_AUTH)
+//        ) {
+//            return '';
+//        }
+        return parent::_toHtml();
+    }
+}
