bfadca5657b9e2c38bf146d437e5896d057b68fc
Sergii Shkiria <s.shkiria@astoundcommerce.com>
2015-11-22 17:29:25 +0200

    4.7.3. Create a multi-select product attribute from an upgrade data method .

---
+++ app/code/Training/Orm/Setup/UpgradeData.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Training\Orm\Setup;
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Setup\CategorySetup;
+use Magento\Eav\Model\Entity\Attribute\Backend\ArrayBackend;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\UpgradeDataInterface;
+use Magento\Catalog\Setup\CategorySetupFactory;
+use Magento\Store\Model\StoreManagerInterface as StoreManager;
+
+class UpgradeData implements UpgradeDataInterface
+{
+    /**
+     * @var CategorySetupFactory
+     */
+    private $catalogSetupFactory;
+    /**
+     * @var StoreManager
+     */
+    private $storeManager;
+    public function __construct(
+        CategorySetupFactory $categorySetupFactory,
+        StoreManager $storeManager
+    ) {
+        $this->catalogSetupFactory = $categorySetupFactory;
+        $this->storeManager = $storeManager;
+    }
+
+    /**
+     * Upgrades data for a module
+     *
+     * @param ModuleDataSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
+    public function upgrade(ModuleDataSetupInterface $setup,
+                            ModuleContextInterface $context
+    ) {
+        $dbVersion = $context->getVersion();
+        if (version_compare($dbVersion, '0.1.1', '<')) {
+            $admin = $this->storeManager->getStore('admin')->getId();
+            $default = $this->storeManager->getStore('default')->getId();
+            /** @var CategorySetup $catalogSetup */
+            $catalogSetup = $this->catalogSetupFactory->create(['setup' => $setup]);
+            $catalogSetup->addAttribute(Product::ENTITY, 'example_multiselect', [
+                'label' => 'Example Multi-Select',
+                'input' => 'multiselect',
+                'backend' => ArrayBackend::class,
+                'visible_on_front' => 1,
+                'required' => 0,
+                'option' => [
+                    'order' => ['option1' => 10, 'option2' => 20],
+                    'value' => [
+                        'option1' => [
+                            $admin => 'Admin Label 1',
+                            $default => 'Frontend Label 1'
+                        ],
+                        'option2' => [
+                            $admin => 'Admin Label 2',
+                            $default => 'Frontend Label 2'
+                        ],
+                    ]
+                ]
+            ]);
+        }
+    }
+
+}
